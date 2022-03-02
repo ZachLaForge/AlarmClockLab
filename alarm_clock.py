162acaaa6c6ac511ede0adc6f326e6e75075a889
Zach <zach.laforge@hotmail.com>
2022-03-02 14:33:25 -0500

    AlarmClock Class created and several functions

---
+++ alarm_clock.py
@@ -0,0 +1,15 @@
+
+class AlarmClock:
+
+    def __init__(self):
+        self.current_time = '2:00pm'
+        self.alarm_on_or_off = ''
+        self.alarm_set_to ='4:00pm'
+    
+    def time_update(self, updated_time): # Set / Change Current time
+        self.current_time = updated_time
+    
+
+    def power_on_or_off_alarm(self):
+    
+    
