988313832f52df955b7b8b5144c671bf11173a78
Zach <zach.laforge@hotmail.com>
2022-03-02 16:29:11 -0500

    fixed syntax and minor updates

--- AlarmClock.py
+++ AlarmClock.py
@@ -3,14 +3,25 @@ class AlarmClock:
 
     def __init__(self):
         self.current_time = '2:00pm'
-        self.alarm_on_or_off = 'True'
+        self.alarm_on_or_off = 'False'
         self.alarm_set_to ='4:00pm'
     
+
     def time_update(self, updated_time): # Set / Change Current time
         self.current_time = updated_time
-        print(updated_time)
+        print(f' The current time is now {self.current_time}')
 
-    def toggle_alarm_on_or_off(self):
-        is_on = True
-        if is_on == True:
-            print(f'This alarm is on and set to{}')
+
+    def alarm_toggle(self):
+        if self.alarm_on_or_off == False:
+            self.alarm_on_or_off = True
+            print(f'Your alarm is on')
+        else:
+            print(f' Your alarm is off ')
+
+
+    def set_alarm(self, time):
+        self.alarm_set_to = time
+        print(f' The alarm is now set to:{self.alarm_set_to}')
+
+
--- main.py
+++ main.py
@@ -1,1 +1,8 @@
 from AlarmClock  import AlarmClock
+
+
+clock = AlarmClock()
+
+print(f'Your clocks current time is {clock.current_time}')
+
+clock.time_update()
