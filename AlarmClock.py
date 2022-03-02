c85aa1f10aea8b364d5b0112e00228da6963cff3
Zach <zach.laforge@hotmail.com>
2022-03-02 15:01:37 -0500

    Updated file name

--- AlarmClock.py
+++ AlarmClock.py
@@ -3,13 +3,14 @@ class AlarmClock:
 
     def __init__(self):
         self.current_time = '2:00pm'
-        self.alarm_on_or_off = ''
+        self.alarm_on_or_off = 'True'
         self.alarm_set_to ='4:00pm'
     
     def time_update(self, updated_time): # Set / Change Current time
         self.current_time = updated_time
-    
+        print(updated_time)
 
-    def power_on_or_off_alarm(self):
-    
-    
+    def toggle_alarm_on_or_off(self):
+        is_on = True
+        if is_on == True:
+            print(f'This alarm is on and set to{}')
